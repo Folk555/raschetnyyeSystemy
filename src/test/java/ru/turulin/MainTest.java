bf844d2b79928e8070a2dabe1de036645959ecab
Folk555 <sanya.turulin.98@list.ru>
2023-07-18 21:55:21 +0300

    Добавлены негативные тесты

--- src/test/java/ru/turulin/MainTest.java
+++ src/test/java/ru/turulin/MainTest.java
@@ -4,17 +4,17 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class MainTest {
 
@@ -47,5 +47,24 @@ class MainTest {
         Map<String, List<Integer>> actualDuplicates = Main.getDuplicates(array);
 
         Assertions.assertEquals(expectedMap, actualDuplicates);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"promt4.txt,promt4Result.txt",
+            "promt5.txt,promt5Result.txt"})
+    void negative_getDuplicates(String req, String ans) throws IOException {
+        String rootPathString = "src/test/resources/";
+        Path requestFilePath = Path.of(rootPathString + req);
+        Path answerFilePath = Path.of(rootPathString + ans);
+        String[] array = Files.readAllLines(requestFilePath).toArray(new String[0]);
+        Map<String, List<Integer>> expectedMap = Files.readAllLines(answerFilePath).stream()
+                .collect(Collectors.toMap(
+                        e -> e.split(":")[0],
+                        e -> Arrays.stream(e.split(":")[1].split(",")).map(Integer::valueOf).toList()
+                ));
+
+        Map<String, List<Integer>> actualDuplicates = Main.getDuplicates(array);
+
+        Assertions.assertNotEquals(expectedMap, actualDuplicates);
     }
 }
